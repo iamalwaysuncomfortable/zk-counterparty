eb8a00847f01c3963ec60d1d54b49ab4fe60e0fa
iamalwaysuncomfortable <zaphrod.beeblebrox@gmail.com>
2022-09-06 09:42:54 -0400

    Add proof logic

--- applied-crypto-examples/src/lib.rs
+++ applied-crypto-examples/src/lib.rs
@@ -5,4 +5,5 @@ mod merlin_non_interactive_proof;
 pub use crate::{
     config::{ConfigArgs, Tutorials},
     merlin_basics::merlin_basics_tutorial,
+    merlin_non_interactive_proof::{Error, SimpleSchnorrProof},
 };
