93999e9c412f5de8c822218353a114fccc1afb4b
iamalwaysuncomfortable <zaphrod.beeblebrox@gmail.com>
2022-11-28 19:10:38 -0500

    Rename crypto examples crate

--- applied-crypto-references/src/bin/tutorial.rs
+++ applied-crypto-references/src/bin/tutorial.rs
@@ -1,7 +1,7 @@
 //! Demonstrating the usage of Merlin STROBE based transcripts for creating non-interative
 //! public coin arguments and consistent hashing schemes.
 
-use applied_crypto_examples::{ConfigArgs, Tutorials};
+use applied_crypto_references::{ConfigArgs, Tutorials};
 use clap::Parser;
 use merlin_example::{merlin_basics_tutorial, merlin_non_interactive_proof_tutorial};
 
